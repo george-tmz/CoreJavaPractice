17eb805fbaedf71a943306a0ae51b62d8538e807
George <jobsteng@163.com>
2021-01-31 20:45:39 +0800

    update

--- src/ch04/Employee.java
+++ src/ch04/Employee.java
@@ -1,5 +1,43 @@
 package ch04;
 
-public class Employee {
+import java.time.LocalDate;
 
+public class Employee {
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+    private static int nextId = 1;
+    private int id;
+
+    public Employee(String n, double s, int year, int month, int day) {
+        name = n;
+        salary = s;
+        hireDay = LocalDate.of(year, month, day);
+    }
+
+    public void setId() {
+        id = nextId;
+        nextId++;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public LocalDate getHireDay() {
+        return hireDay;
+    }
+
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
 }
---
+++ src/ch04/StaticTest.java
@@ -0,0 +1,17 @@
+package ch04;
+
+public class StaticTest {
+    public static void main(String[] args) {
+        Employee[] staff = new Employee[3];
+        staff[0] = new Employee("Carl Cracker", 75000, 1987, 12, 15);
+        staff[1] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
+        staff[2] = new Employee("Tony Tester", 40000, 1990, 3, 15);
+
+        for (Employee e : staff){
+            e.setId();
+            System.out.println("name=" + e.getName() + ",salary=" + e.getSalary() + ",id="
+                    + e.getId());
+        }
+
+    }
+}
