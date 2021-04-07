736ccff51f66f9c92af8809149431139abc83966
georgeT <jobsteng@163.com>
2021-04-07 11:13:56 +0800

    update

--- src/ch14/bounceThread/BounceThread.java
+++ src/ch14/bounceThread/BounceThread.java
@@ -1,14 +1,26 @@
 package ch14.bounceThread;
 
-import javax.swing.*;
-
 public class BounceThread {
-    public static void main(String[] args) {
+    private static int i = 0;
 
+    public static void main(String[] args) {
+        for (int j = 0; j < 10; j++) {
+            new Thread(BounceThread::add).start();
+        }
+    }
+
+    private static void add() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        i++;
+        System.out.println(i);
     }
 }
 
-class BounceFrame extends JFrame
-{
-//    private
-}
+//class BounceFrame extends JFrame
+//{
+////    private
+//}
